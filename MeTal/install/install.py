5e7e6b87bb93e275254919f35165c7da20dd3cfa
Serdar Yegulalp <serdar@genjipress.com>
2015-08-07 23:18:18 -0400

    format cleanup, some renaming

--- MeTal/install/install.py
+++ MeTal/install/install.py
@@ -3,7 +3,7 @@ from core.boot import settings as _settings
 import os, random, string
 
 from configparser import ConfigParser, DuplicateSectionError
-config_file_name = (_settings.APPLICATION_PATH + _settings.DATA_FILE_PATH + 
+config_file_name = (_settings.APPLICATION_PATH + _settings.DATA_FILE_PATH +
     os.sep + "install.ini")
 parser = ConfigParser()
 
@@ -11,23 +11,23 @@ class SetupError(BaseException):
     pass
 
 def generate_key(N):
-    
+
     return ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(N))
 
 def read_ini(p):
-    
+
     try:
         with open(config_file_name, "r", encoding='utf-8') as input_file:
             p.read_file(input_file)
     except:
         return None
-    
+
     return p
 
 def get_ini(section, option):
-    
+
     read_ini(parser)
-    
+
     try:
         value = parser.get(section, option)
     except:
@@ -35,220 +35,220 @@ def get_ini(section, option):
     return value
 
 def store_ini(section, setting, value):
-    
+
     read_ini(parser)
-    
+
     try:
         parser.add_section(section)
     except DuplicateSectionError:
         pass
-    
+
     parser.set(section, setting, value)
-    
+
     try:
-        with open(config_file_name, "w", encoding='utf-8') as output_file: 
+        with open(config_file_name, "w", encoding='utf-8') as output_file:
             parser.write(output_file)
     except BaseException as e:
         raise SetupError(str(e.__class__.__name__) + ": " + str(e))
 
 def step_0_pre():
-    
+
     path_to_check = _settings.APPLICATION_PATH + _settings.DATA_FILE_PATH
     if os.path.isdir(path_to_check) is False:
         os.makedirs(path_to_check)
-        
+
     with open(path_to_check + _settings._sep + "__init__.py", "w", encoding='utf-8') as output_file:
-        output_file.write('')        
-    
+        output_file.write('')
+
     store_ini('main', 'INSTALL_STEP', '0')
     store_ini('key', 'PASSWORD_KEY', generate_key(32))
     store_ini('key', 'SECRET_KEY', generate_key(32))
-    
+
     return {}
 
 def step_0_post():
-    
+
     return {}
 
 def step_1_pre():
-    
+
     store_ini('main', 'INSTALL_STEP', '1')
-    
+
     email = get_ini("user", "email")
     password = get_ini("user", "password")
-    
+
     if email is None:
         email = ""
-        
+
     if password is None:
         password = ""
-    
+
     return {'email':email,
         'password':password,
         'password_confirm':password
         }
-    
+
 
 def step_1_post():
     step_error = []
 
     user_email = request.forms.getunicode('input_email')
     user_password = request.forms.getunicode('input_password')
-    
+
     if user_email == "":
         step_error.append("You must provide a valid email address.")
 
     if user_password == "" or len(user_password) < 8:
         step_error.append ("Your password cannot be blank or less than eight characters.")
-        
+
     from core.libs.bottle import touni
-    
+
     existing_password = get_ini("main", "password")
     new_password = user_password
-    
+
     if (existing_password == "" or
         existing_password != new_password):
         existing_password = new_password
-        
+
     if existing_password != request.forms.getunicode('input_password_confirm'):
         step_error.append('Your password and password confirmation did not match.')
-        
+
     if len(step_error) > 0:
         raise SetupError('\n'.join(step_error))
-        
-    store_ini("user", "password", touni(existing_password))    
+
+    store_ini("user", "password", touni(existing_password))
     store_ini("user", "email", user_email)
     store_ini('main', 'INSTALL_STEP', '2')
-    
-    return {}    
+
+    return {}
 
 def step_2_pre():
-    
+
     domain = get_ini("path", "base_url_root")
-    if domain is None: 
+    if domain is None:
         domain = "http://" + request.environ['HTTP_HOST']
-        
-        
+
+
     install_path = _settings.APPLICATION_PATH
     blog_path = install_path.rsplit(os.sep, 1)[0]
     cms_path = "/" + install_path.rsplit(os.sep, 1)[1]
-    
+
     return {'domain':domain,
         'install_path':install_path,
         'cms_path':cms_path,
         'blog_path':blog_path}
-    
+
 def step_2_post():
-    
+
     domain = request.forms.getunicode('input_domain')
     install_path = request.forms.getunicode('install_path')
     blog_path = request.forms.getunicode('blog_path')
     cms_path = request.forms.getunicode('cms_path')
-    
+
     store_ini('path', 'base_url_root', domain)
     store_ini('path', 'base_url_path', cms_path)
     store_ini('install', 'install_path', install_path)
     store_ini('install', 'blog_path', blog_path)
-    
+
     store_ini('main', 'INSTALL_STEP', '3')
-    
+
     return {}
 
 def step_3_pre():
-    
+
     return {'db_address':'localhost',
         'db_port':3306,
         'db_name':'mysql',
         'db_username':'',
         'db_password':''}
-    
-    
+
+
 def step_3_post():
 
     database_type = request.forms.getunicode('dbtype')
-    
+
     if database_type == 'sqlite':
-        store_ini('db', 'db_type_name', 'sqlite')        
-        
+        store_ini('db', 'db_type_name', 'sqlite')
+
     if database_type == 'mysql':
         store_ini('db', 'db_type_name', 'mysql')
-    
+
     store_ini('main', 'INSTALL_STEP', '4')
-    
+
     return {'dbtype', database_type}
-    
+
 def step_4_pre():
-    
+
     if get_ini('main', 'DO_DB_CHECK') is None:
         store_ini('main', 'DO_DB_CHECK', 'Y')
         from core.boot import reboot
         reboot()
-    
+
     report = []
-    
+
     from core.models import db, Template
     try:
         db.connect()
     except:
         raise
-    
+
     db.close()
-    
+
     report.append("Database connection successful.")
-    
+
     # create the database
     from settings import DB
     DB.recreate_database()
-    
+
     report.append("Database tables created successfully.")
-    
+
     # create the user account
-    
+
     username = "Administrator"
     email = get_ini("user", "email")
     password = get_ini("user", "password")
     blog_path = get_ini("install", "blog_path")
-    
+
     from core.utils import encrypt_password
     p_key = get_ini('key', 'PASSWORD_KEY')
     password = encrypt_password(password, p_key)
 
     from core import mgmt
-    
+
     # create the first site
     db.connect()
-    
+
     with db.atomic():
-        
+
         from core.models import System
         system_ref = System()
-        
+
         system_ref.add_kv(
             object='System',
             objectid=0,
             key='MaxPageRevisions',
             value=20,
             is_schema=True,
-            is_unique=True)       
-        
+            is_unique=True)
+
         system_ref.add_kv(
             object='System',
             objectid=0,
             key='AutomaticallyReleaseLocksAfter',
             value=3600,
             is_schema=True,
-            is_unique=True)          
-    
+            is_unique=True)
+
         new_site = mgmt.site_create(
             name="Your first site",
             description="The description for your first site.",
             url=get_ini('path', 'base_url_root'),
             path=blog_path)
-        
+
         report.append("Initial site created successfully.")
-        
+
         # create a blog within that site
-        
+
         new_blog = mgmt.blog_create(
             site=new_site,
             name="Your first blog",
@@ -256,76 +256,76 @@ def step_4_pre():
             url=new_site.url,
             path=new_site.path
             )
-        
-        new_user = mgmt.create_user(
+
+        new_user = mgmt.user_create(
             name='Administrator',
             email=email,
             encrypted_password=password)
-        
+
         new_user.save()
-        
+
         from core.auth import role
-        
+
         new_user_permissions = mgmt.add_user_permission(
             new_user,
             permission=role.SYS_ADMIN,
             site=new_site
             )
-        
+
         new_user_permissions.save()
-        
+
         report.append("Initial blog created successfully.")
-        
+
         # make this into a mgmt function
-        
+
         # make into a common variable?
-        install_directory = (_settings.APPLICATION_PATH + _settings._sep + 
+        install_directory = (_settings.APPLICATION_PATH + _settings._sep +
             "install")
-    
+
         with open(install_directory + _settings._sep + "templates.json", "rb") as json_file:
                 json_text = json_file.read()
-        
-        new_theme = mgmt.install_theme_to_system(json_text)
-        
-        report.append("Theme created and installed successfully to system.") 
-        
-        mgmt.install_theme_to_blog(new_theme, new_blog)
-        
+
+        new_theme = mgmt.theme_install_to_system(json_text)
+
+        report.append("Theme created and installed successfully to system.")
+
+        mgmt.theme_install_to_blog(new_theme, new_blog)
+
         report.append("Theme installed in new blog successfully.")
-        
-        
-        plugindir = (_settings.APPLICATION_PATH + _settings._sep + 'data' + 
+
+
+        plugindir = (_settings.APPLICATION_PATH + _settings._sep + 'data' +
             _settings._sep + 'plugins')
-        
+
         import shutil
-        
+
         if (os.path.isdir(plugindir)):
             shutil.rmtree(plugindir)
-            
+
         shutil.copytree(install_directory + _settings._sep + 'plugins',
             plugindir)
-        
+
         from core import plugins
-        
+
         for x in os.listdir(plugindir):
             if (os.path.isdir(plugindir + _settings._sep + x) is True and
                 x != '__pycache__'):
                 plugins.register_plugin(x)
 
         # TODO: export installed theme to data directory
-        
-    
+
+
     db.close()
-    
-    #raise
-    
-    output_file_name = (_settings.APPLICATION_PATH + _settings.DATA_FILE_PATH + 
+
+    # raise
+
+    output_file_name = (_settings.APPLICATION_PATH + _settings.DATA_FILE_PATH +
         os.sep + "config.ini")
-    
+
     config_parser = ConfigParser()
-        
+
     sections = ('db', 'path', 'key')
-    
+
     for s in sections:
         for name, value in parser.items(s):
             try:
@@ -333,34 +333,34 @@ def step_4_pre():
             except DuplicateSectionError:
                 pass
             config_parser.set(s, name, value)
-    
+
     if request.environ['HTTP_HOST'] == _settings.DEFAULT_LOCAL_ADDRESS + _settings.DEFAULT_LOCAL_PORT:
         config_parser.add_section('server')
         config_parser.set('server', 'DESKTOP_MODE', 'True')
-        
+
     try:
-        with open(output_file_name, "w", encoding='utf-8') as output_file: 
+        with open(output_file_name, "w", encoding='utf-8') as output_file:
             config_parser.write(output_file)
     except BaseException as e:
         raise SetupError(str(e.__class__.__name__) + ": " + str(e))
-    
+
     try:
         os.remove(config_file_name)
     except FileNotFoundError:
         pass
-    
+
     finished = '''
     <p>Installation is complete.
     '''
-        
+
     return {'report':report,
         'finished':finished}
 
 def step_4_post():
-    
+
     from core.boot import reboot
     reboot()
-    
+
     # return {}
 
 tpl = '''
@@ -395,16 +395,16 @@ crumb_template = '''
 
 def button(step, next_action, error=None):
 
-        
+
     if step > 0:
         previous = '''
 <a href="{}/install/step-{}"><button type="button" class="btn">&lt;&lt; Go back</button></a>
 '''.format(_settings.BASE_URL_PATH, step - 1)
     else:
         previous = ""
-        
+
     if next_action is not None:
-        
+
         next_str = '''
 <button action="submit" class="btn">Continue &gt;&gt;</button>
 '''
@@ -471,9 +471,9 @@ This will be used to identify the administrator on this installation.
       <input type="password" class="form-control" id="input_password_confirm" name="input_password_confirm" placeholder="Password"
       value ="{{password_confirm}}">
     </div>
-  </div>  
+  </div>
 {{!button}}
-</form>     
+</form>
 '''
         },
 
@@ -489,11 +489,11 @@ This will be used to identify the administrator on this installation.
         <li><label for="install_path">the path on the server to those files,</a>
         <li><label for="blog_path">and the path on the server to where the files for your first
         blog are to be placed.</a>
-        </ul> 
+        </ul>
         <p>We've made a best guess as to what these might be, so if you're not sure about what to do here,
         just press Continue. You can always change them later.
 <form action="{{form_action}}"  method="post" class="form-horizontal">
-  
+
   <div class="form-group">
     <label for="input_domain" class="col-sm-2 control-label">Domain name</label>
     <div class="col-sm-7">
@@ -501,7 +501,7 @@ This will be used to identify the administrator on this installation.
       value="{{domain}}">
     </div>
   </div>
-  
+
     <div class="form-group">
     <label for="cms_path" class="col-sm-2 control-label">URL path to application</label>
     <div class="col-sm-7">
@@ -509,7 +509,7 @@ This will be used to identify the administrator on this installation.
       value="{{cms_path}}">
     </div>
   </div>
-  
+
   <div class="form-group">
     <label for="install_path" class="col-sm-2 control-label">Server installation path</label>
     <div class="col-sm-7">
@@ -517,14 +517,14 @@ This will be used to identify the administrator on this installation.
       value="{{install_path}}">
     </div>
   </div>
-  
+
   <div class="form-group">
     <label for="blog_path" class="col-sm-2 control-label">Path on disk for first site/blog</label>
     <div class="col-sm-7">
       <input type="input" class="form-control" id="blog_path" name="blog_path" placeholder="Path"
       value="{{blog_path}}">
     </div>
-  </div>  
+  </div>
 {{!button}}
 </form>'''},
 
@@ -560,14 +560,14 @@ This will be used to identify the administrator on this installation.
                 <input type="input" class="form-control" id="db_port" name="db_port" placeholder="IP port for database"
                 value="{{db_port}}">
             </div>
-        </div>        
+        </div>
         <div class="form-group">
             <label for="db_name" class="col-sm-2 control-label">Database name</label>
             <div class="col-sm-7">
                 <input type="input" class="form-control" id="db_name" name="db_name" placeholder="Name of MySQL database"
                 value="{{db_name}}">
             </div>
-        </div>        
+        </div>
         <div class="form-group">
             <label for="db_user" class="col-sm-2 control-label">MySQL username</label>
             <div class="col-sm-7">
@@ -581,7 +581,7 @@ This will be used to identify the administrator on this installation.
             <input type="password" class="form-control" id="db_password" name="db_password" placeholder="Password for database user"
             value ="{{db_password}}">
             </div>
-        </div>                
+        </div>
     </div>
 {{!button}}
 </form>
@@ -610,34 +610,34 @@ This will be used to identify the administrator on this installation.
     }
 
 def crumbs(step):
-    
+
     crumbs = []
-    
+
     m = 0
     while True:
         try:
             n = step_text[m]
         except KeyError:
             break
-        
+
         if m > step:
             crumb = crumb_element_active.format(n['crumb'])
         else:
             crumb = crumb_element_link.format(_settings.BASE_URL_PATH, m, n['crumb'])
-        
+
         crumbs.append(crumb)
         m += 1
-        
-        # http://docs.quantifiedcode.com/python-code-patterns/readability/using_an_unpythonic_loop.html        
-        
+
+        # http://docs.quantifiedcode.com/python-code-patterns/readability/using_an_unpythonic_loop.html
+
     return crumb_template.format(''.join(crumbs))
 
 def step(step):
-    
+
     error_msg = None
-    
+
     if request.method == "POST":
-         
+
         try:
             results = step_text[step]['post-step']()
         except SetupError as e:
@@ -646,7 +646,7 @@ def step(step):
         else:
             step += 1
             redirect('{}/install/step-{}'.format(_settings.BASE_URL_PATH, step))
-            
+
     else:
         try:
             results = step_text[step]['pre-step']()
@@ -658,7 +658,7 @@ def step(step):
         template_button = button(step, step_text[step]['next_action'])
     else:
         template_button = button(step, None, error_msg)
-        
+
     return template(tpl,
         settings=_settings,
         step=step,
