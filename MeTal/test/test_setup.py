f1e64c90fc8d81470dbc54c8a90815506e0d3097
Serdar Yegulalp <serdar@genjipress.com>
2015-08-07 23:18:47 -0400

    function renaming

--- MeTal/test/test_setup.py
+++ MeTal/test/test_setup.py
@@ -5,50 +5,50 @@ import settings
 from core import mgmt
 
 class SetupTest(unittest.TestCase):
-    
-    
+
+
     def setUp(self):
 
         # tear down and create new DB
-        
+
         from core.models import init_db
         from core.auth import role
-        
+
         init_db.recreate_database()
-        
-        new_user = mgmt.create_user(
+
+        new_user = mgmt.user_create(
             name="Administrative User",
             email="admin.user@test-domain.com",
-            password="password",            
+            password="password",
             )
-        
-        mgmt.add_user_permission(new_user, 
-            permission = role.SYS_ADMIN,
-            site = 0)
-        
+
+        mgmt.add_user_permission(new_user,
+            permission=role.SYS_ADMIN,
+            site=0)
+
         # create a site
         # create a blog
-        # create users for each, assign permissions 
-        
+        # create users for each, assign permissions
+
     def test_create_admin_user(self):
         print ("hi")
         pass
-        
+
     def test_create_site(self):
-        
+
         pass
-    
+
     def test_create_site_user(self):
-        
+
         pass
-    
+
     def test_create_blog(self):
-        
+
         pass
-    
+
     def test_create_blog_user(self):
-        
-        pass    
+
+        pass
 
 if __name__ == '__main__':
-    unittest.main()   
+    unittest.main()
