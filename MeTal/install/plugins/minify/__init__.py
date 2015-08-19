f021712ba4ee3c21468023f21049698e9b40312d
Serdar Yegulalp <serdar@genjipress.com>
2015-08-18 20:29:09 -0400

    added sample sidebar in edit screen to minify plugin behavior

--- MeTal/install/plugins/minify/__init__.py
+++ MeTal/install/plugins/minify/__init__.py
@@ -6,19 +6,37 @@ __version__ = '0.1'
 __license__ = 'MIT'
 __compatibility__ = 0
 
+
 def install():
     pass
 
 def load():
-    
+
+    sidebar = {
+            'add_to':'edit_page',
+            'panel':{
+                'template':'\nThis is a test!',
+                'title':'Test',
+                'label':'test',
+                'icon':'picture'
+                },
+
+            }
+
     from .lib import minify
-    
+
     return ({
-        'action':'after',
-        'module':'core.cms',
-        'function':'generate_page_text',
-        'wrap':minify
-        },)
-    
+            'action':'after',
+            'module':'core.cms',
+            'function':'generate_page_text',
+            'wrap':minify
+        },
+        {
+            'action':'exec',
+            'module':'core.ui_mgr',
+            'function':'register_sidebar',
+            'data':sidebar
+            })
+
 def uninstall():
     pass
