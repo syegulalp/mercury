2a245bae4540e4a266b932eb5910c2cb04913bc8
Serdar Yegulalp <serdar@genjipress.com>
2015-08-21 22:18:45 -0400

    minor code cleanup for consistency w/new schema

--- MeTal/core/theme.py
+++ MeTal/core/theme.py
@@ -1,6 +1,6 @@
 import json
 from core.libs.playhouse.shortcuts import model_to_dict
- 
+
 def default(obj):
     import datetime
 
@@ -8,64 +8,65 @@ def default(obj):
         return datetime.datetime.strftime(obj, '%Y-%m-%d %H:%M:%S')
 
 def json_dump(obj):
-    
+
     return json.loads(json.dumps(model_to_dict(obj, recurse=False),
             default=default,
             separators=(', ', ': '),
             indent=1))
 
 def export_theme_for_blog(blog_id):
-    
-    from core.models import Template, TemplateMapping, KeyValue
 
-    theme_to_export = Template.select().where(
-        Template.blog == blog_id)
-    
+    from core.models import KeyValue, get_blog
+
+    # theme_to_export = Template.select().where(Template.blog == blog_id)
+    blog = get_blog(blog_id)
+    theme_to_export = blog.templates
+
     theme = {}
     theme["title"] = theme_to_export[0].theme.title
     theme["description"] = theme_to_export[0].theme.description
     theme["data"] = {}
-    
+
     for n in theme_to_export:
         theme["data"][n.id] = {}
         theme["data"][n.id]["template"] = json_dump(n)
-        
-        mappings_to_export = TemplateMapping.select().where(
-            TemplateMapping.template == n)
-        
+
+        # mappings_to_export = TemplateMapping.select().where(TemplateMapping.template == n)
+        mappings_to_export = n.mappings
+
         theme["data"][n.id]["mapping"] = {}
-        
+
         for m in mappings_to_export:
             theme["data"][n.id]["mapping"][m.id] = json_dump(m)
 
     theme["kv"] = {}
-    
+
     kv_list = []
-    
+
     top_kvs = KeyValue.select().where(
         KeyValue.object == 'Theme',
         KeyValue.objectid == theme_to_export[0].theme.id,
         KeyValue.is_schema == True)
-    
-    for n in top_kvs:        
+
+    for n in top_kvs:
         kv_list.append(n)
-    
+
     while len(kv_list) > 0:
-        theme["kv"][kv_list[0].id] = json_dump(kv_list[0]) 
+        theme["kv"][kv_list[0].id] = json_dump(kv_list[0])
         next_kvs = KeyValue.select().where(
             KeyValue.parent == kv_list[0],
             KeyValue.is_schema == True)
         for f in next_kvs:
             kv_list.append(f)
         del kv_list[0]
-        
+
     import settings
-    with open(settings.APPLICATION_PATH + settings._sep + "install" + settings._sep + 
+    with open(settings.APPLICATION_PATH + settings._sep + "install" + settings._sep +
         "templates.json", "w", encoding='utf-8') as output_file:
         output_file.write(json.dumps(theme,
             indent=1,
             sort_keys=True,
             allow_nan=True))
-        
+
     return theme
 
