562e033204e61548fe0411f74709ce0b17929be8
Serdar Yegulalp <serdar@genjipress.com>
2015-08-25 18:22:48 -0400

    rewritten transaction model!

--- MeTal/core/models/transaction.py
+++ MeTal/core/models/transaction.py
@@ -1,9 +1,11 @@
 from core.error import LoggedException
 from functools import wraps
 from core.libs.bottle import request
+from core.libs.peewee import OperationalError
 from core.models import db
-from settings import DB
+from settings import DB, DATABASE_RETRIES, RETRY_INTERVAL
 from sys import exc_info
+from time import sleep
 
 DBError, error_text = DB.db_warnings()
 
@@ -11,13 +13,28 @@ def transaction(func):
     @wraps(func)
     def wrapper(*a, **ka):
         db.connect()
-        try:
-            with db.atomic():
-                fn = func(*a, **ka)
-        except LoggedException as e:
-            raise exc_info()[0](e.msg)
-        except DBError as e:
-            raise LoggedException(error_text.format(e, request.url))           
-        db.close()
+        n = 0
+        while n < DATABASE_RETRIES:
+            try:
+                with db.atomic():
+                    fn = func(*a, **ka)
+            except OperationalError as e:
+                n += 1
+                if n >= DATABASE_RETRIES:
+                    raise e
+                else:
+                    sleep(RETRY_INTERVAL)
+                    continue
+            except LoggedException as e:
+                raise exc_info()[0](e.msg)
+            except DBError as e:
+                raise LoggedException(error_text.format(e, request.url))
+            except BaseException as e:
+                raise e
+            else:
+                db.close()
+                break
+
         return fn
+
     return wrapper
