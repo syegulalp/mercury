ee276d0930c122ee080745ac91e27ee334a0ee3c
Serdar Yegulalp <serdar@genjipress.com>
2015-09-23 18:05:27 -0400

    added template revisions to database creation

--- MeTal/core/models/init_db.py
+++ MeTal/core/models/init_db.py
@@ -1,29 +1,29 @@
 import settings
 def recreate_database():
-	
+
 	from core.models import (db, User, Site, Blog, Page, PageCategory,
 		KeyValue, Tag, TagAssociation, Category,
-		Theme, Template, TemplateMapping, Media, FileInfo,
-		Queue, Permission, MediaAssociation, PageRevision, FileInfoContext, Plugin, Log,)	
-	
+		Theme, Template, TemplateRevision, TemplateMapping, Media, FileInfo,
+		Queue, Permission, MediaAssociation, PageRevision, FileInfoContext, Plugin, Log,)
+
 	db.connect()
 
 	with db.atomic():
-	
+
 		db.drop_tables((User, Site, Blog, Page, PageCategory,
 			KeyValue, Tag, TagAssociation, Category,
-			Theme, Template, TemplateMapping, Media, FileInfo,
+			Theme, Template, TemplateRevision, TemplateMapping, Media, FileInfo,
 			Queue, Permission, MediaAssociation, PageRevision, FileInfoContext, Plugin, Log),
 			safe=True)
-		
+
 		db.create_tables((User, Site, Blog, Page, PageCategory,
 			KeyValue, Tag, TagAssociation, Category,
-			Theme, Template, TemplateMapping, Media, FileInfo,
+			Theme, Template, TemplateRevision, TemplateMapping, Media, FileInfo,
 			Queue, Permission, MediaAssociation, PageRevision, FileInfoContext, Plugin, Log),
 			safe=False)
-		
+
 		settings.DB.create_index_table()
 
 		db.execute_sql(settings.DB.post_recreate())
-	
+
 	db.close()
