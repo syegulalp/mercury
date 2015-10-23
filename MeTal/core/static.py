09c2479c6e6035e852bdfcfc70ac78af05cff3bd
Serdar Yegulalp <serdar@genjipress.com>
2015-10-23 18:58:36 -0400

    Added static settings for HTML editor

--- MeTal/core/static.py
+++ MeTal/core/static.py
@@ -1,4 +1,3 @@
-
 editor_css = '''
 body, td, pre {color:#000; font-family: serif;
     font-size:16px;}
@@ -46,5 +45,62 @@ div.screenshot.screenshot-double img
 codemirror_css = '''
 .CodeMirror {
     height: auto;
+}
+'''
+
+html_editor_settings = '''
+{
+    init_instance_callback: function() {
+
+    },
+    setup: function(ed) {
+        ed.on('init', function(args) {
+            window.onbeforeunload = leave;
+        });
+        ed.on('change', function() {
+            window.onbeforeunload = stay;
+        });
+    },
+    selector: "textarea.editor",
+    convert_urls: false,
+    entity_encoding: "named",
+    browser_spellcheck: true,
+    cleanup_on_startup: true,
+    content_css: global.base + '/blog/' + global.blog + '/editor-css',
+    plugins: [
+        "advlist save autosave link image lists hr anchor pagebreak",
+        "searchreplace wordcount visualblocks visualchars code fullscreen media nonbreaking",
+        "table contextmenu template textcolor paste colorpicker textpattern"
+    ],
+    style_formats: [{
+        title: 'Standard graf',
+        block: 'p'
+    }, {
+        title: 'Head',
+        block: 'p',
+        classes: 'lead'
+    }, {
+        title: 'Small',
+        block: 'small'
+    }, {
+        title: 'Code',
+        inline: 'code'
+    }, ],
+    toolbar1: "save | styleselect formatselect | bold italic underline strikethrough removeformat | bullist numlist | outdent indent blockquote hr | link unlink anchor image | visualchars visualblocks nonbreaking template pagebreak | code fullscreen",
+
+    save_enablewhendirty: true,
+    save_onsavecallback: function() {
+        $("#save_button").click();
+    },
+    menubar: false,
+    toolbar_items_size: 'small',
+
+    templates: [{
+        title: 'Test template 1',
+        content: 'Test 1'
+    }, {
+        title: 'Test template 2',
+        content: 'Test 2'
+    }]
 }
 '''
