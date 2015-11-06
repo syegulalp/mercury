76279161ff5fcc83f5726d4c5aff7cd328a54636
Serdar Yegulalp <serdar@genjipress.com>
2015-11-06 13:30:21 -0500

    incorrect order of operations

--- MeTal/install/plugins/minify/lib.py
+++ MeTal/install/plugins/minify/lib.py
@@ -1,11 +1,10 @@
 import re
 
 minifications = (
+    (re.compile(r'\n', re.M), r' '),
     (re.compile(r'\s{2,}', re.M), r' '),
-    (re.compile(r'\n', re.M), r''),
     (re.compile(r'\t', re.M), r''),
     (re.compile(r'> <', re.M), r'><'),)
-
 
 def minify(text):
 
@@ -14,6 +13,4 @@ def minify(text):
     for rgx, rpl in minifications:
         n = re.sub(rgx, rpl, n)
 
-    text = n
-
-    return text
+    return n
