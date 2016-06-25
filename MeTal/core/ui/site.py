4d2ecda603153e92dd898bc681325b305847c23a
Serdar Yegulalp <serdar@genjipress.com>
2016-06-25 16:29:21 -0400

    remove old code

--- MeTal/core/ui/site.py
+++ MeTal/core/ui/site.py
@@ -36,33 +36,3 @@ def site(site_id, errormsg=None):
         {'site_id':site.id}
         )
 
-    """
-    try:
-        blogs_searched, search = site_search_results(request, site)
-    except (KeyError, ValueError):
-        blogs_searched, search = None, None
-
-    # page = utils.page_list_id(request)
-
-    tags = template_tags(site_id=site.id,
-        search=search,
-        user=user)
-
-    if errormsg is not None:
-        tags.status = errormsg
-
-    taglist = tags.site.blogs
-
-    paginator, rowset = utils.generate_paginator(taglist, request)
-
-    tpl = template('listing/listing_ui',
-        paginator=paginator,
-        search_context=(search_context['site'], site),
-        menu=generate_menu('site_menu', site),
-        rowset=rowset,
-        colset=colsets['site'],
-        icons=icons,
-        **tags.__dict__)
-
-    return tpl
-    """
