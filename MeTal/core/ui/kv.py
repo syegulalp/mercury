fc65f88976f64d44416c470d55db7522510796b0
Serdar Yegulalp <serdar@genjipress.com>
2016-07-13 19:06:54 -0400

    removed ui_kv.py

--- MeTal/core/ui/kv.py
+++ MeTal/core/ui/kv.py
@@ -2,13 +2,14 @@ from core.models.transaction import transaction
 from core.libs.bottle import (template)
 
 def kv_response(object_name, object_type, object_identifier, object_id):
-    from core.ui_kv import kv_ui
+    # from core.ui_kv import kv_ui
+    from core.ui import kv
     from core.models import template_tags
 
     tag_args = {}
     tag_args[object_identifier] = object_id
     tags = template_tags(**tag_args)
-    kv_ui_data = kv_ui(tags.__dict__[object_type].kv_list())
+    kv_ui_data = kv.ui(tags.__dict__[object_type].kv_list())
 
     tpl = template('sidebar/kv',
         kv_ui=kv_ui_data,
@@ -85,3 +86,68 @@ def kv_remove():
 
     return kv_response(object_type.object, objmap[object_type.object][0],
         objmap[object_type.object][1], object_instance.id)
+
+
+from core.utils import html_escape
+
+blank_item = '''
+<li class="list-group-item wrap-txt">
+{}<a onclick="remove_kv({});" href="#" title="Remove item">
+<span class="pull-right glyphicon glyphicon-remove media-remove"></span></a></li>
+'''
+# <span title="Select {}" class="glyphicon glyphicon-plus-sign media-selector"></span>
+
+media_item = '''
+<li class="list-group-item" data-toggle="tooltip" data-placement="left"
+      data-html="true"
+      title="<div style='background-color:white'><img style='max-height:50px;' src='{}'></div>">
+      <span
+      title="Select media"
+      class="glyphicon glyphicon-plus-sign media-selector"></span>
+      {}:
+      <a href="#" title="Remove media"><span class="pull-right glyphicon glyphicon-remove media-remove"></span></a>
+      <a target="_blank" href="{}">{}</a>
+</li>'''
+
+def ui(keys):
+
+    if keys is None:
+        return None
+
+    kv_ui = []
+
+    for n in keys:
+
+        kv_ui.append(blank_item.format(
+                # "text",
+                "<b>{}:</b> <i>{}</i>".format(html_escape(n.key), html_escape(n.value)),
+                n.id))
+
+        '''
+        if n.is_schema is True:
+            m = n
+            # chooser only
+            kv_ui.append(blank_item.format(
+                "text",
+                "{}: {}".format(n.key, n.value)))
+        else:
+            # chooser and existing kv
+            m = n.key_parent if n.key_parent is not None else n
+
+            if m.value_type == "Media":
+                from core.models import Media
+                p = Media.get(Media.id == n.value)
+                kv_ui.append(media_item.format(
+                    p.preview_url,
+                    m.value,
+                    p.link_format,
+                    p.friendly_name))
+            elif m.value_type == "User":
+                from core.models import get_user
+                user_obj = get_user(user_id=int(n.value))
+                kv_ui.append(blank_item.format(
+                    "user",
+                    "{}: {}".format(m.value, user_obj.name)))
+        '''
+
+    return ''.join(kv_ui)
