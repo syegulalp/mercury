00f2b7a5bf0acfd8b11557f97116d42ee482f07b
Serdar Yegulalp <serdar@genjipress.com>
2016-07-16 17:01:36 -0400

    revised preview format for pages

--- MeTal/core/ui/page.py
+++ MeTal/core/ui/page.py
@@ -248,11 +248,11 @@ def page_preview(page_id):
     else:
         page_url = preview_fileinfo.url.rsplit('/', 1)[0] + '/' + preview_file
 
-    tpl = template('ui/ui_preview',
-        page=page,
-        page_url=page_url)
-
-    return tpl
+    response.add_header('Refresh', "0,{}?_={}".format(
+        page_url,
+        page.modified_date.microsecond
+        ))
+    return response
 
 @transaction
 def delete_page_preview(page_id):
